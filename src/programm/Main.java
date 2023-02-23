2f2063320806bd7f8746af42c64b8d11bd61c378
Blind-Hawk <romanzimmer99@gmail.com>
2023-02-23 13:55:23 +0100

    v0.1.1
    Tested a possible command line for future uses.

--- src/programm/Main.java
+++ src/programm/Main.java
@@ -2,12 +2,36 @@ package programm;
 import weapons.firearm;
 import weaponparts.magazine;
 
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        firearm testGun = new firearm("TestGun",9.19, 0, false);
-        magazine testMag = new magazine("TestMag", 9.19, 10);
-        System.out.println(testMag.loadMag(20) + "\n");
-        System.out.println(testMag.unloadMag(5) + "\n");
-        System.out.println(testMag.loadMag(4) + "\n");
+        Scanner userInput = new Scanner(System.in);
+
+        System.out.println("Welcome to the text based Weapon Simulator made by Roman Zimmer");
+        System.out.println("Just follow the following requests to make and use your weapon \n");
+        System.out.println("Now what is the name of your weapon?");
+        System.out.print("Type here:");
+        String weaponName = userInput.nextLine();
+
+        System.out.println("What is the weapon class of your weapon? (for example pistol, rifle, shotgun)");
+        System.out.print("Type here:");
+        String weaponClass = userInput.nextLine();
+
+        System.out.println("What is the caliber of your weapon? (for example 9mm, 5.56mm, 12gauge)");
+        System.out.print("Type here:");
+        double caliber = userInput.nextDouble();
+
+        System.out.println("What is the firemode of your weapon? (for example 1 = semi, 2 = burst, 3 = auto)");
+        System.out.print("Type here:");
+        int firemode = userInput.nextInt();
+
+        System.out.println("Is your weapon foldable? (for example true = foldable, false = not foldable)");
+        System.out.print("Type here:");
+        boolean foldable = userInput.nextBoolean();
+        userInput.close();
+
+        firearm weapon = new firearm(weaponName, weaponClass, caliber, firemode, foldable);
+        System.out.println("Your weapon is now created!");
     }
 }
--- src/weapons/firearm.java
+++ src/weapons/firearm.java
@@ -1,20 +1,26 @@
 package weapons;
 
 public class firearm {
-    private String name;
+    private String weaponName;
+    private String weaponClass;
     private double caliber;
     private int firemode;
     private boolean foldable;
 
-    public firearm(String name, double caliber, int firemode, boolean foldable) {
-        this.name = name;
+    public firearm(String name, String weaponClass, double caliber, int firemode, boolean foldable) {
+        this.weaponName = name;
+        this.weaponClass = weaponClass;
         this.caliber = caliber;
         this.firemode = firemode;
         this.foldable = foldable;
     }
 
-    public String getName() {
-        return name;
+    public String getWeaponName() {
+        return weaponName;
+    }
+
+    public String getWeaponClass() {
+        return weaponClass;
     }
 
     public double getCaliber() {
